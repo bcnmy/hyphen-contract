3e37082f976a812950f080ce260b3928b5a5f0c7
Ankur Dubey <ankurdubey521@protonmail.com>
2022-02-27 16:14:12 +0400

    Update ILPToken.sol

--- contracts/hyphen/interfaces/ILPToken.sol
+++ contracts/hyphen/interfaces/ILPToken.sol
@@ -78,7 +78,7 @@ interface ILPToken {
 
     function totalSupply() external view returns (uint256);
 
-    function abitransferFrom(
+    function transferFrom(
         address from,
         address to,
         uint256 tokenId
