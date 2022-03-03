270e6566ef01132a597e13fd2f960611e7668b26
Ankur Dubey <ankurdubey521@protonmail.com>
2022-03-03 21:57:46 +0400

    Update deployment scripts

--- scripts/deploy-goerli.ts
+++ scripts/deploy-goerli.ts
@@ -4,8 +4,8 @@ import { deploy } from "./helpers";
 (async () => {
   const config = {
     trustedForwarder: "0xE041608922d06a4F26C0d4c27d8bCD01daf1f792",
-    bicoOwner: "0x817377683A2899A6879eA12AaC60e68032D3748F",
-    pauser: "0x817377683A2899A6879eA12AaC60e68032D3748F",
+    bicoOwner: "0x46b65ae065341D034fEA45D76c6fA936EAfBfdeb",
+    pauser: "0x46b65ae065341D034fEA45D76c6fA936EAfBfdeb",
     tokens: [
       {
         tokenAddress: "0x64ef393b6846114bad71e2cb2ccc3e10736b5716",
@@ -15,8 +15,32 @@ import { deploy } from "./helpers";
         equilibriumFee: 10000000,
         maxFee: 200000000,
         maxWalletLiquidityCap: ethers.BigNumber.from(10).pow(18 + 4),
-        maxLiquidityCap: ethers.BigNumber.from(10).pow(18 + 5),
+        maxLiquidityCap: ethers.BigNumber.from(10).pow(18 + 6),
         svgHelper: await ethers.getContractFactory("EthereumUSDT"),
+        decimals: 18,
+      },
+      {
+        tokenAddress: "0xb5B640E6414b6DeF4FC9B3C1EeF373925effeCcF",
+        minCap: ethers.BigNumber.from(10).pow(6 + 2),
+        maxCap: ethers.BigNumber.from(10).pow(6 + 4),
+        toChainIds: [43113, 80001],
+        equilibriumFee: 100,
+        maxFee: 2000,
+        maxWalletLiquidityCap: ethers.BigNumber.from(10).pow(6 + 4),
+        maxLiquidityCap: ethers.BigNumber.from(10).pow(6 + 6),
+        svgHelper: await ethers.getContractFactory("EthereumUSDC"),
+        decimals: 6,
+      },
+      {
+        tokenAddress: "0xDdc47b0cA071682e8dc373391aCA18dA0Fe28699",
+        minCap: ethers.BigNumber.from(10).pow(18 + 2),
+        maxCap: ethers.BigNumber.from(10).pow(18 + 4),
+        toChainIds: [43113, 80001],
+        equilibriumFee: 10000000,
+        maxFee: 200000000,
+        maxWalletLiquidityCap: ethers.BigNumber.from(10).pow(18 + 4),
+        maxLiquidityCap: ethers.BigNumber.from(10).pow(18 + 6),
+        svgHelper: await ethers.getContractFactory("EthereumBICO"),
         decimals: 18,
       },
       {
@@ -27,7 +51,7 @@ import { deploy } from "./helpers";
         equilibriumFee: 10000000,
         maxFee: 200000000,
         maxWalletLiquidityCap: ethers.BigNumber.from(10).pow(18 + 4),
-        maxLiquidityCap: ethers.BigNumber.from(10).pow(18 + 5),
+        maxLiquidityCap: ethers.BigNumber.from(10).pow(18 + 6),
         svgHelper: await ethers.getContractFactory("EthereumETH"),
         decimals: 18,
       },
