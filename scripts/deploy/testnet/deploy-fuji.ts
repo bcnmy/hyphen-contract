24cc4c4b01290d60a6fdf0ce735dd17854c25d7c
Ankur Dubey <ankurdubey521@protonmail.com>
2022-06-13 16:36:53 +0530

    LiquidityFarmingV2 and Optimism Changes

--- scripts/deploy/testnet/deploy-fuji.ts
+++ scripts/deploy/testnet/deploy-fuji.ts
@@ -1,20 +1,21 @@
 import { ethers } from "hardhat";
 import { parseUnits } from "ethers/lib/utils";
-import { deploy, IDeployConfig } from "./helpers";
+import { deploy } from "../deploy-utils";
+import type { IDeployConfig } from "../../types";
 
 (async () => {
   const config: IDeployConfig = {
-    trustedForwarder: "0xE041608922d06a4F26C0d4c27d8bCD01daf1f792",
+    trustedForwarder: "0x6271Ca63D30507f2Dcbf99B52787032506D75BBF",
     bicoOwner: "0x46b65ae065341D034fEA45D76c6fA936EAfBfdeb",
     pauser: "0x46b65ae065341D034fEA45D76c6fA936EAfBfdeb",
     tokens: [
       {
-        tokenAddress: "0x64ef393b6846114bad71e2cb2ccc3e10736b5716",
+        tokenAddress: "0xB4E0F6FEF81BdFea0856bB846789985c9CFf7e85",
         minCap: ethers.BigNumber.from(10).pow(18 + 2),
         maxCap: ethers.BigNumber.from(10).pow(18 + 4),
         depositConfigs: [
           {
-            chainId: 43113,
+            chainId: 5,
             minCap: ethers.BigNumber.from(10).pow(18 + 2),
             // Max Cap needs to be less than the maxTransfer Fee on destination chain id to cover for incentive amount
             maxCap: ethers.BigNumber.from(9).mul(ethers.BigNumber.from(10).pow(18 + 3)),
@@ -31,11 +32,11 @@ import { deploy, IDeployConfig } from "./helpers";
         transferOverhead: 0,
         maxWalletLiquidityCap: ethers.BigNumber.from(10).pow(18 + 4),
         maxLiquidityCap: ethers.BigNumber.from(10).pow(18 + 5),
-        svgHelper: await ethers.getContractFactory("EthereumUSDT"),
+        svgHelper: await ethers.getContractFactory("AvalancheUSDT"),
         decimals: 18,
         rewardRatePerSecond: 100,
-        rewardTokenAddress: "0x64ef393b6846114bad71e2cb2ccc3e10736b5716",
-        excessStateTransferFeePer: parseUnits("0.045", 8),
+        rewardTokenAddress: "0xB4E0F6FEF81BdFea0856bB846789985c9CFf7e85",
+        excessStateTransferFeePer: parseUnits("0.045", 8)
       },
       {
         tokenAddress: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
@@ -43,13 +44,13 @@ import { deploy, IDeployConfig } from "./helpers";
         maxCap: ethers.BigNumber.from(10).pow(18 + 2),
         depositConfigs: [
           {
-            chainId: 43113,
+            chainId: 80001,
             minCap: ethers.BigNumber.from(10).pow(18 - 2),
             // Max Cap needs to be less than the maxTransfer Fee on destination chain id to cover for incentive amount
             maxCap: ethers.BigNumber.from(97).mul(ethers.BigNumber.from(10).pow(18)),
           },
           {
-            chainId: 80001,
+            chainId: 5,
             minCap: ethers.BigNumber.from(10).pow(18 - 2),
             // Max Cap needs to be less than the maxTransfer Fee on destination chain id to cover for incentive amount
             maxCap: ethers.BigNumber.from(97).mul(ethers.BigNumber.from(10).pow(18)),
@@ -60,34 +61,11 @@ import { deploy, IDeployConfig } from "./helpers";
         transferOverhead: 0,
         maxWalletLiquidityCap: ethers.BigNumber.from(10).pow(18 + 4),
         maxLiquidityCap: ethers.BigNumber.from(10).pow(18 + 5),
-        svgHelper: await ethers.getContractFactory("EthereumETH"),
+        svgHelper: await ethers.getContractFactory("AvalancheETH"),
         decimals: 18,
         rewardRatePerSecond: 100,
-        rewardTokenAddress: "0x64ef393b6846114bad71e2cb2ccc3e10736b5716",
-        excessStateTransferFeePer: parseUnits("0.045", 8),
-      },
-      {
-        tokenAddress: "0xb5B640E6414b6DeF4FC9B3C1EeF373925effeCcF",
-        minCap: ethers.BigNumber.from(10).pow(6 + 2),
-        maxCap: ethers.BigNumber.from(10).pow(6 + 4),
-        depositConfigs: [
-          {
-            chainId: 80001,
-            minCap: ethers.BigNumber.from(10).pow(6 + 2),
-            // Max Cap needs to be less than the maxTransfer Fee on destination chain id to cover for incentive amount
-            maxCap: ethers.BigNumber.from(9).mul(ethers.BigNumber.from(10).pow(6 + 3)),
-          },
-        ],
-        equilibriumFee: 10000000,
-        maxFee: 200000000,
-        transferOverhead: 0,
-        maxWalletLiquidityCap: ethers.BigNumber.from(10).pow(6 + 4),
-        maxLiquidityCap: ethers.BigNumber.from(10).pow(6 + 5),
-        svgHelper: await ethers.getContractFactory("EthereumUSDC"),
-        decimals: 6,
-        rewardRatePerSecond: 100,
-        rewardTokenAddress: "0x64ef393b6846114bad71e2cb2ccc3e10736b5716",
-        excessStateTransferFeePer: parseUnits("0.045", 8),
+        rewardTokenAddress: "0xB4E0F6FEF81BdFea0856bB846789985c9CFf7e85",
+        excessStateTransferFeePer: parseUnits("0.045", 8)
       },
     ],
   };
--- scripts/upgrades/upgrade-whitelist-period-manager.ts
+++ scripts/upgrades/upgrade-whitelist-period-manager.ts
@@ -1,13 +1,13 @@
 import { upgradeWhiteListPeriodManager } from "./upgrade";
-import { verifyImplementation } from "../helpers";
+import { verifyImplementation } from "../deploy/deploy-utils";
 import { ethers } from "hardhat";
 
 (async () => {
-  const proxy = "0x62A0521d3F3B75b70fA39926A0c63CBf819870a6";
+  const proxy = process.env.PROXY || "0x62A0521d3F3B75b70fA39926A0c63CBf819870a6";
   const [signer] = await ethers.getSigners();
   console.log("Proxy: ", proxy, " Deployer: ", signer.address);
   console.log("Upgrading Proxy...");
-  await upgradeWhiteListPeriodManager(proxy);
   await new Promise((resolve) => setTimeout(resolve, 5000));
+  await upgradeWhiteListPeriodManager(proxy);
   await verifyImplementation(proxy);
 })();
