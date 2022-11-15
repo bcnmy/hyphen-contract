9dd0316ce93f2995b1844a31a89eed202fa86e5e
tomarsachin2271 <sachin.tomar@biconomy.io>
2022-11-15 18:16:59 +0530

    Adding transfer overhead script

--- scripts/transferOverhead.js
+++ scripts/transferOverhead.js
@@ -1,31 +1,31 @@
 let { BigNumber, ethers } = require("ethers");
 
 // This amount should be get from exit transaction parameters including reward amount
-let sentAmountDecimal = "100.034865722000000000";
-let receivedAmountDecimal = "99.117062201702917424";
+let sentAmountDecimal = "1000.044162380608210561";
+let receivedAmountDecimal = "999.263696621337790679";
 let differenceInAmount = sentAmountDecimal - receivedAmountDecimal;
 
 // Set the transfer Fee percentrage deducted in this transaction. Can be fetched from db or exit transaction logs on explorer.
-let transferFeePerc = 0.1
+let transferFeePerc = 0.045;
 // let transferFee = sentAmountDecimal * transferFeePerc / 100;
 
-let transferFee = 0.101002733058320060;
+let transferFee = 0.779819937184043763;
 let expectedExecutorFeeInToken = differenceInAmount - transferFee;
 
 console.log(expectedExecutorFeeInToken);
 // If token is USDC, then what is the price of native token in USDC?
-let nativeTokenPriceInToken = 1959.65;
+let nativeTokenPriceInToken = 0.5049751243781094527363184079;
 
 let expectedExecutorFeeInNative = expectedExecutorFeeInToken / nativeTokenPriceInToken;
-console.log(expectedExecutorFeeInNative)
-let gasFeeDeductedOnChain =  ethers.utils.parseUnits(expectedExecutorFeeInNative.toFixed(6), "18");
+console.log(expectedExecutorFeeInNative);
+let gasFeeDeductedOnChain = ethers.utils.parseUnits(expectedExecutorFeeInNative.toFixed(6), "18");
 
 // Set the gas fee used by the executor. Can be fetched from explorer
-let actualGasFeeByExecutor = ethers.utils.parseUnits("0.000687140002198848", "18");
+let actualGasFeeByExecutor = ethers.utils.parseUnits("0.0014251622211", "18");
 let gasUnaccountedFor = BigNumber.from(actualGasFeeByExecutor).sub(gasFeeDeductedOnChain);
 
 // Final gas price of the trasnaction. Can be fetched from the explorer
-let gasPrice = ethers.utils.parseUnits("2.500000008","9");
+let gasPrice = ethers.utils.parseUnits("5.9043", "9");
 
 let transferOverhead = gasUnaccountedFor.div(gasPrice);
 
