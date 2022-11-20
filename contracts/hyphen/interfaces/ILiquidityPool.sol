4e9fd76dc4cdb4f8d0f12d78f58304f9e7b28eb1
Ankur Dubey <ankurdubey521@protonmail.com>
2022-11-21 02:29:34 +0530

    Update interfaces

--- contracts/hyphen/interfaces/ILiquidityPool.sol
+++ contracts/hyphen/interfaces/ILiquidityPool.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 import "../structures/DepositAndCall.sol";
 
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 interface ILiquidityPool {
     function depositErc20(
