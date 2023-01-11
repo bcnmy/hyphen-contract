9dccf3314cc01223e48c90a2b154fc72c9832a95
divyan73 <divya@biconomy.io>
2023-01-11 12:28:59 +0400

    calculate transferOverhead script

--- scripts/transferOverhead.js
+++ scripts/transferOverhead.js
@@ -1,31 +1,29 @@
 let { BigNumber, ethers } = require("ethers");
 
 // This amount should be get from exit transaction parameters including reward amount
-let sentAmountDecimal = "1000.044162380608210561";
-let receivedAmountDecimal = "999.263696621337790679";
+let sentAmountDecimal = "54.878029";
+let receivedAmountDecimal = "54.677275";
 let differenceInAmount = sentAmountDecimal - receivedAmountDecimal;
 
 // Set the transfer Fee percentrage deducted in this transaction. Can be fetched from db or exit transaction logs on explorer.
-let transferFeePerc = 0.045;
-// let transferFee = sentAmountDecimal * transferFeePerc / 100;
 
-let transferFee = 0.779819937184043763;
+let transferFee = 0.043692;
 let expectedExecutorFeeInToken = differenceInAmount - transferFee;
 
 console.log(expectedExecutorFeeInToken);
 // If token is USDC, then what is the price of native token in USDC?
-let nativeTokenPriceInToken = 0.5049751243781094527363184079;
+let nativeTokenPriceInToken = 1259.3370000000000000;
 
 let expectedExecutorFeeInNative = expectedExecutorFeeInToken / nativeTokenPriceInToken;
 console.log(expectedExecutorFeeInNative);
 let gasFeeDeductedOnChain = ethers.utils.parseUnits(expectedExecutorFeeInNative.toFixed(6), "18");
 
 // Set the gas fee used by the executor. Can be fetched from explorer
-let actualGasFeeByExecutor = ethers.utils.parseUnits("0.0014251622211", "18");
+let actualGasFeeByExecutor = ethers.utils.parseUnits("0.00013417491626", "18");
 let gasUnaccountedFor = BigNumber.from(actualGasFeeByExecutor).sub(gasFeeDeductedOnChain);
 
 // Final gas price of the trasnaction. Can be fetched from the explorer
-let gasPrice = ethers.utils.parseUnits("5.9043", "9");
+let gasPrice = ethers.utils.parseUnits("0.1", "9");
 
 let transferOverhead = gasUnaccountedFor.div(gasPrice);
 
